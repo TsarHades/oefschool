03768d83f2408ea69b7bb538f43a59fc23aa2b67
PC-NICOLAS\nicol <nicolasjacobs2407@gmail.com>
2023-11-30 10:15:42 +0100

    Nog bezig met oef 4 => Laatste twee asserts werken niet volledig

--- Oefening_4.py
+++ Oefening_4.py
@@ -1,16 +1,19 @@
 # creëer hier de gevraagde functie(s) met implementatie:
 def vervang_tekst(zoekterm,woord,vervangterm):
-    if zoekterm in woord:
-        woordsplit = woord.split(zoekterm)
+    woordeind = woord
+    woordsplit2 = woord
+    while zoekterm in woordsplit2:
+        woordsplit = woordeind.split(zoekterm)[0]
         print(woordsplit)
-        woordstuff = woord.split(zoekterm)[0] + vervangterm + woord.split(zoekterm)[1]
-        if "" in woordsplit:
-            woordstuff = woordstuff + vervangterm + woord.[1]
-            print(woordstuff)
+        woordsplit2 = woordeind.split(woordsplit + zoekterm)[1]
+        print(woordsplit2)
+        woordeind = woordsplit + vervangterm + woordsplit2
+        print(woordeind)
+    return woordeind
 
 
 # testen:
-#assert vervang_tekst("ee", "computationeel denken", "aa") == "computationaal denken"
+assert vervang_tekst("ee", "computationeel denken", "aa") == "computationaal denken"
 assert vervang_tekst("ten", "hottentottententententoonstelling",
                      "stuff") == "hotstufftotstuffstuffstuffstufftoonstelling"
 assert vervang_tekst("e", "ee", "ee") == "eeee"
