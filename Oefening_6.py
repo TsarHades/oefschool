101ef48459d130b8b236108431ce38b026127924
PC-NICOLAS\nicol <nicolasjacobs2407@gmail.com>
2023-12-07 17:20:19 +0100

    oef 6 gedaan

--- Oefening_6.py
+++ Oefening_6.py
@@ -6,16 +6,16 @@ def aantal_keren_voorkomen(lijst):
     check = []
     for i in range(0,len(lijst)-1):
         if lijst[i] == lijst[i+1]:
-            print("yes")
             x+=1
         else:
             check.append((lijst[i],int(x)))
             x=1
     check.append((lijst[len(lijst)-1],int(x)))
     print(check)
+    return check
 
 
 # testen:
-assert aantal_keren_voorkomen(["a", "b", "c", "b", "c", "b", "a"]) == [('a', 2), ('b', 3), ('c', 2)]
+assert aantal_keren_voorkomen(["a", "b", "c", "b", "c", "b", "a"]) == [("a", 2), ("b", 3), ("c", 2)]
 assert aantal_keren_voorkomen(["c", "b", "a", "a", "b", "c", "b"]) == [("a", 2), ("b", 3), ("c", 2)]
 assert aantal_keren_voorkomen([1, 4, 2, 3, 2, 9, 1]) == [(1, 2), (2, 2), (3, 1), (4, 1), (9, 1)]
