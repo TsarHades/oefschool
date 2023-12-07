52c2744010b2014d23d9fbdc98f34a48abfc6e6a
PC-NICOLAS\nicol <nicolasjacobs2407@gmail.com>
2023-12-07 15:07:28 +0100

    H2 begonnen met oef 6

---
+++ Oefening_6.py
@@ -0,0 +1,7 @@
+# creëer hier de gevraagde functie(s) met implementatie:
+
+
+# testen:
+assert aantal_keren_voorkomen(["a", "b", "c", "b", "c", "b", "a"]) == [("a", 2), ("b", 3), ("c", 2)]
+assert aantal_keren_voorkomen(["c", "b", "a", "a", "b", "c", "b"]) == [("a", 2), ("b", 3), ("c", 2)]
+assert aantal_keren_voorkomen([1, 4, 2, 3, 2, 9, 1]) == [(1, 2), (2, 2), (3, 1), (4, 1), (9, 1)]
