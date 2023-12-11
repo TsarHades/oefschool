76d2a6efbffba38e2cfd3ccad846d3dfbed14c80
PC-NICOLAS\nicol <nicolasjacobs2407@gmail.com>
2023-12-11 12:11:26 +0100

    Oefening 3.3 deel b en oef 3.4 bezig

--- H3/Oefening_3.3.py
+++ H3/Oefening_3.3.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy
+
+##deel a
 interval = numpy.arange(0,2*numpy.pi,0.01)
 x = numpy.sin(interval)
 y = numpy.sin(interval)
@@ -18,6 +20,13 @@ plt.show()
 
 x = numpy.cos(3*interval)
 y = numpy.sin(interval)
+plt.plot(x,y)
+plt.show()
+
+##deel b
+xtemp = 16*(numpy.sin(interval))
+x = numpy.power(xtemp, 3)
+y = 13*(numpy.cos(interval))-5*(numpy.cos(2*interval)) - 2*numpy.cos(3*interval) - numpy.cos(4*interval)
 plt.plot(x,y)
 plt.show()
 
